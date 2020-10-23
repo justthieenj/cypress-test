6af593a307cf1989c5a4b3215bbb771e32826d81
Thien Nguyen <ngkhanhthien94@gmail.com>
2020-10-24 02:17:04 +0700

    update sample file

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -2,13 +2,14 @@ describe('My First Test', () => {
     it('Gets, types and asserts', () => {
         cy.visit('https://www.google.com')
 
+        // Search
         cy.get('input[name=q]')
             .click()
             .type('Foster The People')
             .type('{enter}')
 
-        // Get an input, type into it and verify that the value has been updated
+        // Verify the first result title
         cy.xpath("//div[@id='search']/div/div/div[@class='g'][1]//h3")
-        .should('have.text', 'Foster the People – Wikipedia tiếng Việt')
+            .should('have.text', 'Foster the People – Wikipedia tiếng Việt')
     })
 })
